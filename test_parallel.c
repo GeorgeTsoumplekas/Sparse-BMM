2e7940ee3f1c608981788ed6882d901df86260b8
GeorgeTsoumplekas <giwrikaspro@gmail.com>
2021-09-28 16:02:12 +0300

    Added new test functions

---
+++ test_parallel.c
@@ -0,0 +1,104 @@
+#include "blocked_bmm_parallel.h"
+
+int main(int argc, char* argv[]){
+    
+    if(argc<5){
+        printf("Not enough arguments.\n");
+        exit(-1);
+    }
+
+    struct timespec begin, end;
+    long seconds;
+    long nanoseconds;
+    double elapsed;
+
+    char* filename_A = argv[1];
+    char* filename_B = argv[2];
+    char* filename_C = argv[3];
+
+    int thread_num = atoi(argv[4]);
+    printf("You have chosen %d threads.\n",thread_num);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    coo_matrix* A_coo = readCOO(filename_A);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for A txt->COO: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    coo_matrix* B_coo = readCOO(filename_B);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for B txt->COO: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    comp_matrix* A_csr = coo2csr(A_coo);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for A COO->CSR: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    comp_matrix* B_csc = coo2csc(B_coo);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for B COO->CSC: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    comp_matrix* C_csr = nonblocked_bmm_parallel(A_csr,B_csc,thread_num);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for bmm: %.5f seconds.\n", elapsed);
+
+    // Check result
+    uint32_t check = check_result(filename_C,C_csr);
+    if(check==0){
+        printf("Wrong result.\n");
+    }
+    else{
+        printf("Correct result.\n");
+    }
+
+    free_coo(A_coo);
+    free_coo(B_coo);
+
+    free_comp_matrix(A_csr);
+    free_comp_matrix(B_csc);
+    free_comp_matrix(C_csr);
+
+    return 0;
+}
---
+++ test_serial.c
@@ -0,0 +1,101 @@
+#include "bmm_seq.h"
+
+int main(int argc, char* argv[]){
+    
+    if(argc<4){
+        printf("Not enough arguments.\n");
+        exit(-1);
+    }
+
+    struct timespec begin, end;
+    long seconds;
+    long nanoseconds;
+    double elapsed;
+
+    char* filename_A = argv[1];
+    char* filename_B = argv[2];
+    char* filename_C = argv[3];
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    coo_matrix* A_coo = readCOO(filename_A);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for A txt->COO: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    coo_matrix* B_coo = readCOO(filename_B);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for B txt->COO: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    comp_matrix* A_csr = coo2csr(A_coo);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for A COO->CSR: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    comp_matrix* B_csc = coo2csc(B_coo);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for B COO->CSC: %.5f seconds.\n", elapsed);
+
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    comp_matrix* C_csr = bmm_seq(A_csr,B_csc,0);
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    elapsed = seconds + nanoseconds * 1e-9;
+
+    printf("\nTime elapsed for bmm: %.5f seconds.\n", elapsed);
+
+    // Check result
+    uint32_t check = check_result(filename_C,C_csr);
+    if(check==0){
+        printf("Wrong result.\n");
+    }
+    else{
+        printf("Correct result.\n");
+    }
+
+    free_coo(A_coo);
+    free_coo(B_coo);
+
+    free_comp_matrix(A_csr);
+    free_comp_matrix(B_csc);
+    free_comp_matrix(C_csr);
+
+    return 0;
+}
