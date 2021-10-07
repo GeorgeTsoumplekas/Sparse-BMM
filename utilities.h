6fb1ec1780c8352729a5b2ecba4f302ea83d64e3
GeorgeTsoumplekas <giwrikaspro@gmail.com>
2021-10-07 20:53:08 +0300

    Deleted 2d matrix

--- utilities.h
+++ utilities.h
@@ -9,7 +9,9 @@
 #include <string.h>
 #include <time.h>
 
-/* ......   Structs   ......*/
+/* --------------- Structs --------------*/
+
+
 // This struct can either describe a Compressed Sparse Column (CSC) or a
 // Compressed Sparse Row (CSR) matrix format.
 // If it is CSC then the col array has n+1 values and the row array has nnz
@@ -22,14 +24,6 @@ typedef struct {
     uint32_t nnz;
     uint32_t n;
 } comp_matrix;
-
-
-//Struct that holds a dense 2-dimensional matrix
-typedef struct {
-    uint32_t** mat;
-    uint32_t rows;
-    uint32_t cols;
-} matrix_2d;
 
 
 //Struct that holds a matrix in coo format
@@ -54,66 +48,6 @@ typedef struct {
     uint32_t n_b;
     uint32_t real_dim;
 } block_comp_matrix;
-
-
-
-/* -----------------------Functions for the 2d matrices----------------------------*/
-
-
-// Creates a random 2d matrix
-matrix_2d* rand_matrix_2d(int rows, int cols) {
-    matrix_2d* array = (matrix_2d*)malloc(sizeof(matrix_2d));
-    if (array == NULL){
-        printf("Couldn't allocate memory for array in rand_matrix_2d\n");
-        exit(-1);
-    } 
-
-    array->mat = (uint32_t**)malloc(rows * sizeof(uint32_t*));
-    if (array->mat == NULL){
-        printf("Couldn't allocate memory for array->mat in rand_matrix_2d\n");
-        exit(-1);
-    }
-
-    for (uint32_t i = 0; i < rows; i++) {
-        array->mat[i] = (uint32_t*)malloc(cols * sizeof(uint32_t));
-        if (array->mat[i] == NULL){
-            printf("Couldn't allocate memory for array->mat[%d] in rand_matrix_2d\n",i);
-            exit(-1);
-        }   
-    }
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            array->mat[i][j] = (uint32_t)(rand() % 2);
-        }
-    }
-
-    array->cols = cols;
-    array->rows = rows;
-
-    return array;
-}
-
-
-//Prints an array of type matrix_2d
-void print_matrix_2d(matrix_2d* array) {
-    for (int i = 0; i < array->rows; i++) {
-        for (int j = 0; j < array->cols; j++) {
-            printf("%d ", array->mat[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
-//Free memory allocated for a 2d matrix
-void free_matrix_2d(matrix_2d* array) {
-    for (int i = 0; i < array->rows; i++) {
-        free(array->mat[i]);
-    }
-    free(array->mat);
-    free(array);
-}
 
 
 /*------------------------Functions for COO matrices--------------------------------*/
@@ -220,78 +154,6 @@ comp_matrix* new_comp_matrix(uint32_t nnz, uint32_t n, char* type) {
     array->n = n;
 
     return array;
-}
-
-
-// Function that turns a matrix_2d array to csc format
-comp_matrix* matrix2csc(matrix_2d* array) {
-
-    uint32_t nnz = 0;
-
-    // Find the number of the non zero elements
-    for (int i = 0; i < array->rows; i++) {
-        for (int j = 0; j < array->cols; j++) {
-            if (array->mat[i][j] != 0){
-                nnz++;
-            } 
-        }
-    }
-
-    comp_matrix* csc = new_comp_matrix(nnz, (uint32_t)array->cols, "csc");
-
-    int col_array_idx = 1;
-    int row_array_idx = 0;
-
-    csc->col[0] = 0;
-
-    for (int j = 0; j < array->cols; j++) {
-        for (int i = 0; i < array->rows; i++) {
-            if (array->mat[i][j] != 0) {
-                csc->row[row_array_idx] = i;
-                row_array_idx++;
-            }
-        }
-        csc->col[col_array_idx] = row_array_idx;
-        col_array_idx++;
-    }
-
-    return csc;
-}
-
-
-//Function that turns a matrix_2d array to csr format
-comp_matrix* matrix2csr(matrix_2d* array) {
-
-    uint32_t nnz = 0;
-
-    // Find the number of the non zero elements
-    for (int i = 0; i < array->rows; i++) {
-        for (int j = 0; j < array->cols; j++) {
-            if (array->mat[i][j] != 0){
-                nnz++;
-            }
-        }
-    }
-
-    comp_matrix* csr = new_comp_matrix(nnz, (uint32_t)array->cols, "csr");
-
-    int col_array_idx = 0;
-    int row_array_idx = 1;
-
-    csr->row[0] = 0;
-
-    for (int i = 0; i < array->rows; i++) {
-        for (int j = 0; j < array->cols; j++) {
-            if (array->mat[i][j] != 0) {
-                csr->col[col_array_idx] = j;
-                col_array_idx++;
-            }
-        }
-        csr->row[row_array_idx] = col_array_idx;
-        row_array_idx++;
-    }
-
-    return csr;
 }
 
 
